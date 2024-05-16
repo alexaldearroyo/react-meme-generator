3289396ff8fecb1aec7732e977723b4a2f119a69
alexaldearroyo <alexaldearroyo@gmail.com>
2024-05-17 00:37:03 +0200

    Update TemplateSelector component design and sub-components

--- src/TemplateSelector.js
+++ src/TemplateSelector.js
@@ -1,8 +1,27 @@
 import './TemplateSelector.css';
 import React from 'react';
 
+const SelectorText = () => {
+  return (
+    <input
+      type="text"
+      className="selector-text"
+      placeholder="Search templates"
+    />
+  );
+};
+
+const SelectorButton = () => {
+  return <button className="selector-button">Search</button>;
+};
+
 const TemplateSelector = () => {
-  return <div className="template-selector">Meme Template Selector</div>;
+  return (
+    <div className="template-selector">
+      <SelectorText />
+      <SelectorButton />
+    </div>
+  );
 };
 
 export default TemplateSelector;
