d52a67e0431782844ae8f45be20414c87826a198
alexaldearroyo <alexaldearroyo@gmail.com>
2024-05-26 15:56:44 +0200

    Update code

--- src/App.js
+++ src/App.js
@@ -25,9 +25,7 @@ function App() {
 
   const handleShowImage = (templateName, topText, bottomText) => {
     if (templates.includes(templateName)) {
-      const encodedTopText = encodeURIComponent(topText.toLowerCase());
-      const encodedBottomText = encodeURIComponent(bottomText.toLowerCase());
-      const newImageUrl = `https://api.memegen.link/images/${templateName}/${encodedTopText}/${encodedBottomText}.png`;
+      const newImageUrl = `https://api.memegen.link/images/${templateName}/${topText}/${bottomText}.png`;
       setImageUrl(newImageUrl);
       setShowImage(true);
       setErrorMessage('');
