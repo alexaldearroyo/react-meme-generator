9fa3cf241b9cf9d205c4e2576e8ad9a20cdebc1c
alexaldearroyo <alexaldearroyo@gmail.com>
2024-05-26 15:51:57 +0200

    Undo last change

--- src/App.js
+++ src/App.js
@@ -25,11 +25,8 @@ function App() {
 
   const handleShowImage = (templateName, topText, bottomText) => {
     if (templates.includes(templateName)) {
-      const encodedTopText = encodeURIComponent(topText).replace(/%20/g, ' ');
-      const encodedBottomText = encodeURIComponent(bottomText).replace(
-        /%20/g,
-        ' ',
-      );
+      const encodedTopText = encodeURIComponent(topText.toLowerCase());
+      const encodedBottomText = encodeURIComponent(bottomText.toLowerCase());
       const newImageUrl = `https://api.memegen.link/images/${templateName}/${encodedTopText}/${encodedBottomText}.png`;
       setImageUrl(newImageUrl);
       setShowImage(true);
